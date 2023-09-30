e52188d196dffa6867f565682b77dbc09d87df6c
Hydrant <ido.k.19980@gmail.com>
2023-09-30 22:13:28 +0300

    Feature: Download Trailers
    no can download trailers from .nfo

--- mgmt.py
+++ mgmt.py
@@ -1,7 +1,10 @@
-import os, sys
+from pytube import YouTube, exceptions as yt_exceptions
+import os, sys, time
 import subprocess
 import audio_codecs
+import xml.etree.ElementTree as ET
 
+YOUTUBE_URL = "https://www.youtube.com/watch?v="
 
 ignore = [
     ".git",
@@ -10,7 +13,6 @@ ignore = [
 
 endpoint_folders = [
     "1080p",
-    "Trailers",
     "Subs",
     "Extras",
     "Behind The Scenes",
@@ -26,6 +28,7 @@ global_state = {
     "deleted_nfos": [],
     "moved_trailers": [],
     "no_trailers": [],
+    "has_trailers": [],
     "all_movs": [],
     "to_convert": [],
     "converted_trailers": [],
@@ -48,6 +51,9 @@ def log(section: str, values: list):
         global_state["moved_trailers"].append(values[1])
     elif section == "no_trailers":
         global_state["no_trailers"].append("'"+values[0] + "' lacks trailers")
+        global_state["log"].append(values[0])
+    elif section == "has_trailers":
+        global_state["has_trailers"].append("'"+values[0] + "' has trailers")
         global_state["log"].append(values[0])
     elif section == "convertion_succeed":
         global_state["log"].append("'"+values[0] + "' converted to '" + values[1]+"'")
@@ -80,6 +86,46 @@ def delete_nfo(path: str):
         os.remove(path)
         log("deleted_nfos", [path])
 
+def manage_nfo(path: str, flags: dict):
+    if os.path.isfile(path) and path[-4:] == ".nfo":
+        if flags["delete_nfos"]:
+            delete_nfo(path)
+        if flags["youtube"]:
+            youtube(path)
+
+def youtube(path: str):
+    tree = ET.parse(path)
+    trailers = tree.findall('.//trailer')
+    for trailer in trailers:
+        youtube_id = trailer.text.split("videoid=")[-1]
+        if not " " in youtube_id:
+            link = YOUTUBE_URL + youtube_id
+            folders = path.split("\\")
+            filename = folders[-1][:-4] + "-trailer.mp4"
+            save_path = "\\".join(folders[:-1])
+            try:
+                # object creation using YouTube
+                # which was imported in the beginning
+                yt = YouTube(link, use_oauth=False, allow_oauth_cache=False)
+            except Exception: 
+                print("Connection Error")
+
+            try:
+                stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1]
+                try:
+                    # downloading the video
+                    stream.download(output_path=save_path, filename=filename)
+                    print('Trailer Downloaded!!')
+                except Exception: 
+                    print("Some Error!")
+            except yt_exceptions.AgeRestrictedError:
+                print('Trailer is age restricted')
+            except yt_exceptions.VideoRegionBlocked:
+                print('Trailer is blocked in this region')
+            except yt_exceptions.VideoPrivate:
+                print('Trailer is Private')
+            except yt_exceptions.VideoUnavailable:
+                print('Trailer is Unavilable')
 
 def move_trailers(file_path: str):
     if '-trailer.' in file_path:
@@ -101,12 +147,14 @@ def move_trailers(file_path: str):
 
 
 def no_trailer(path: str):
-    if path.split("\\")[-1] not in endpoint_folders:
+    directory_name = path.split("\\")[-1]
+    if directory_name not in endpoint_folders:
         def check_further(path: str):
             no_trailer_files = True
             for file in os.listdir(path):
                 if os.path.isfile and "-trailer" in file:
                     no_trailer_files = False
+                    log("has_trailers", [path])
                     break
             if no_trailer_files:
                 log("no_trailers", [path])
@@ -127,13 +175,12 @@ def convert_trailers(dir_path: str, filename: str):
             if ".avi" in filename.lower():
                 _format = ".avi"
             if ".mov" in filename.lower():
-                _format = ".mov"
-                
+                _format = ".mov"              
             input_file = os.path.join(dir_path, filename)
             output_file = os.path.join(dir_path, filename.replace(_format, ".mp4"))
             subprocess.call(['ffmpeg', '-i', input_file, output_file])
             log("convertion_succeed", [input_file, output_file])
-        except:
+        except Exception:
             log("convertion_failed", [input_file])
 
 
@@ -162,9 +209,8 @@ def main_loop(root: str, flags: dict):
             continue
         else:
             if item_name not in ignore:
-                dir_has_files = True  
-            if flags["delete_nfos"]:
-                delete_nfo(item_path)
+                dir_has_files = True
+            manage_nfo(item_path, flags)
 
             # different functionality for different folder names
             if dir_name == "1080p":
@@ -210,39 +256,44 @@ def main():
         "convert_trailers": False,
         "delete_movs": False,
         "print_movs": False,
-        "print_dts": False
+        "print_dts": False,
+        "youtube": False
     }
 
     inpt = input("""
     1) Remove dots
-    2) Delete .nfo Files
-    3) Move Trailers
-    4) No Trailers
-    5) Convert Trailers to .mp4
-    6) Delete .mov Files
-    7) Print .mov Files
-    8) Print DTS Files
+    2) Download Youtube trailers
+    3) Delete .nfo Files
+    4) Move Trailers
+    5) No Trailers
+    6) Convert Trailers to .mp4
+    7) Delete .mov Files
+    8) Print .mov Files
+    9) Print DTS Files
 
     """)
     print("\n")
     if "1" in inpt:
         flags["remove_dots"] = True
     if "2" in inpt:
-        flags["delete_nfos"] = True
+        flags["youtube"] = True
     if "3" in inpt:
-        flags["move_trailers"] = True
+        flags["delete_nfos"] = True
     if "4" in inpt:
-        flags["no_trailer"] = True
+        flags["move_trailers"] = True
     if "5" in inpt:
-        flags["convert_trailers"] = True
+        flags["no_trailer"] = True
     if "6" in inpt:
-        flags["delete_movs"] = True
+        flags["convert_trailers"] = True
     if "7" in inpt:
-        flags["print_movs"] = True
+        flags["delete_movs"] = True
     if "8" in inpt:
+        flags["print_movs"] = True
+    if "9" in inpt:
         flags["print_dts"] = True
 
     main_loop(root_dir, flags)
+    print("")
 
     if flags["print_movs"]:
         for item in global_state["all_movs"]:
@@ -250,6 +301,9 @@ def main():
         print("")
     if flags["no_trailer"]:
         for item in global_state["no_trailers"]:
+            print(item)
+        print("")
+        for item in global_state["has_trailers"]:
             print(item)
         print("")
     if flags["print_dts"]:
